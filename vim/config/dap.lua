bf33e942ebcc00d07378d160f71f5d45fd7017b2
Reece Stevens <reecestevens24@gmail.com>
2023-04-26 12:37:51 -0500

    Add DAP configuration for attaching to Firefox

--- vim/config/dap.lua
+++ vim/config/dap.lua
@@ -1,4 +1,4 @@
-require('dap')
+local dap = require('dap')
 require('dapui').setup({
     controls = {
         element = "repl",
@@ -6,11 +6,33 @@ require('dapui').setup({
     }
 })
 require('dap-python').setup('~/.vim/python-virtual-env/nvim-venv/bin/python')
-require("nvim-dap-virtual-text").setup()
+-- Disabling virtualtext since it seems to be interfering with copilot plugin
+-- require("nvim-dap-virtual-text").setup()
 
--- map dap continue to leader dc
+dap.adapters.firefox = {
+  type = 'executable',
+  command = '/Users/reecestevens/.nodenv/versions/16.13.2/bin/node',
+  args = {os.getenv('HOME') .. '/.vim/vscode-firefox-debug/dist/adapter.bundle.js'},
+}
+
 vim.api.nvim_set_keymap('n', '<leader>dc', ':lua require"dap".continue()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>db', ':lua require"dap".toggle_breakpoint()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>ds', ':lua require"dap".step_into()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>do', ':lua require"dap".step_over()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>di', ':lua require"dapui".toggle()<CR>', {noremap = true, silent = true})
+
+
+-- Example configuration for firefox debugging. Project-specific configurations should
+-- go in their local .nvimrc file.
+--
+-- dap.configurations["typescript.tsx"] = {
+--   {
+--     name = 'Debug with Firefox',
+--     type = 'firefox',
+--     request = 'launch',
+--     reAttach = true,
+--     url = 'http://localhost:3000',
+--     firefoxExecutable = '/Applications/Firefox.app/Contents/MacOS/firefox',
+--     webRoot = '${workspaceFolder}',
+--   }
+-- }
