9c8c4e1c2dd6643fdf6a0e298789038f21e0d652
Reece Stevens <reecestevens24@gmail.com>
2023-07-05 12:50:23 -0500

    Configure Python LSP for additionally using ruff

--- vim/config/lsp.lua
+++ vim/config/lsp.lua
@@ -60,7 +60,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- local servers = { "pyright", "rust_analyzer", "tsserver" }
-local servers = { "pylsp", "rust_analyzer", "svelte", "clangd"}
+local servers = { "rust_analyzer", "svelte", "clangd", "ruff_lsp"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
@@ -100,6 +100,23 @@ nvim_lsp.tsserver.setup {
       }
     },
     capabilities = capabilities,
+}
+
+nvim_lsp.pylsp.setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    capabilities = capabilities,
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {enabled = false},
+                pyflakes = {enabled = false},
+                mccabe = {enabled = false},
+            }
+        }
+    }
 }
 
 nvim_lsp.diagnosticls.setup {
