e4f13d974ae0d44e320b7d9110c0c6577ffd5a29
Reece Stevens <reecestevens24@gmail.com>
2023-07-05 12:49:03 -0500

    Add conditional breakpoint support

--- vim/config/dap.lua
+++ vim/config/dap.lua
@@ -17,6 +17,7 @@ dap.adapters.firefox = {
 
 vim.api.nvim_set_keymap('n', '<leader>dc', ':lua require"dap".continue()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>db', ':lua require"dap".toggle_breakpoint()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>dB', ':lua require"dap".toggle_breakpoint(vim.fn.input(\'Breakpoint condition: \'))<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>ds', ':lua require"dap".step_into()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>do', ':lua require"dap".step_over()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>di', ':lua require"dapui".toggle()<CR>', {noremap = true, silent = true})
