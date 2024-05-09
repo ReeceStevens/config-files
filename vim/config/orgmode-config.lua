400652d6656f84498921381ccf612c4fce0295f6
Reece Stevens <reecestevens24@gmail.com>
2024-05-09 06:26:20 -0500

    Streamline capture templates

--- vim/config/orgmode-config.lua
+++ vim/config/orgmode-config.lua
@@ -3,10 +3,10 @@ require('orgmode').setup({
   org_default_notes_file = '~/innolitics/notes/org-notes/refile.org',
   org_todo_keywords = {'TODO', 'IN_PROGRESS', 'BLOCKED', 'DONE'},
   org_capture_templates = {
-    t = { description = 'Task', template = '* TODO %?\n  %u' },
-    m = {
-      description = 'Meeting',
-      template = '* %?\n  %u\n\n** Notes\n\n\n** Action Items\n\n\n' }
+    t = {
+        description = 'Task',
+        template = '* TODO %?\nSCHEDULED: %t\n:PROPERTIES:\n:CREATED_ON: %u\n:END:',
+        properties = { empty_lines = 1 } },
   },
   org_startup_indented = false,
   org_startup_folded = 'showeverything',
@@ -26,7 +26,6 @@ require('orgmode-backlinks')
 vim.api.nvim_set_hl(0, '@org.agenda.deadline', {link = 'Error'})
 vim.api.nvim_set_hl(0, '@org.agenda.scheduled', {link = 'Function'})
 vim.api.nvim_set_hl(0, '@org.agenda.scheduled_past', {link = 'DiagnosticWarn'})
-
 vim.api.nvim_set_hl(0, 'OctoEditable', {bg = "#1B1D1F"})
 
 -- To conceal org hyperlinks, which can be somewhat verbose
