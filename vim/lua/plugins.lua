aa39e94b59b42f3fc69ebe71bdd20856e9beceb7
Reece Stevens <reecestevens24@gmail.com>
2025-01-27 04:22:26 -0600

    Open nested nvim sessions in same window

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -603,9 +603,6 @@ require("lazy").setup({
 
       {
         "samjwill/nvim-unception",
-        init = function()
-          vim.g.unception_open_buffer_in_new_tab = true
-        end,
       },
 
       { "ReeceStevens/vim-reviewer" },
