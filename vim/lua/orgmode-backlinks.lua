59ed0fc2f160d2aac6d4acd9d0fbf8049fc2fa79
Reece Stevens <reecestevens24@gmail.com>
2025-04-28 05:28:38 -0500

    Add org-to-markdown conversion command

--- vim/lua/orgmode-backlinks.lua
+++ vim/lua/orgmode-backlinks.lua
@@ -31,3 +31,28 @@ vim.api.nvim_create_user_command(
     end,
     { nargs = 0 }
 )
+
+-- pass the selected range of text into pandoc to convert it to markdown, then save to
+-- clipboard
+vim.api.nvim_create_user_command(
+    "PandocConvertToMarkdown",
+    function(opts)
+        -- Get the start and end positions of the range
+        local start_line = opts.line1
+        local end_line = opts.line2
+
+        -- Get the selected text from the buffer
+        local selected_lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+        local selected_text = table.concat(selected_lines, '\n')
+
+        -- Escape single quotes in the text to prevent shell interpretation issues
+        selected_text = selected_text:gsub("'", "'\\''")
+
+        local pandoc_command = "echo '" .. selected_text .. "' | pandoc -f org -t markdown"
+        local markdown_text = vim.fn.system(pandoc_command)
+        vim.fn.setreg("+", markdown_text)
+    end,
+    { range = true }
+)
+
+vim.api.nvim_set_keymap('v', '<leader>cp', ':PandocConvertToMarkdown<CR>', { noremap = true, silent = true })
